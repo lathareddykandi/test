b85064fb92f5c981a1a47431863a58da68cf253c
Latha Reddy <lathareddy2580@gmail.com>
2022-09-09 15:34:13 +0530

    Even Number

--- Latha.c
+++ Latha.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 main()
 {
+int a,
+printf("Input a number",a);
+if (a%2==0){
+printf("It is a even number");
+}
+else{
+printf("It is odd number")
 printf("Latha's First repository");
+}
 }
